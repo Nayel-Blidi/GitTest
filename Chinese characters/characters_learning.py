86ec505d84dba328cb819e733ce0199e813b01f3
Nayel BLIDI <nayel.blidi@ipsa.fr>
2023-08-05 16:22:53 +0200

    working supervised NN

--- Chinese characters/characters_learning.py
+++ Chinese characters/characters_learning.py
@@ -54,18 +54,49 @@ def ImageListStack(path, image_list):
         
     return np.stack(array_list, axis=2)
 
-if __name__ == "__main__" and ( (len(sys.argv) <= 1) or ("tensor" in sys.argv) ):     # Generates tensor data only if no terminal argv, or "tensor" argument
+def ConvolutedImageListStack(path, image_list):
+    
+    array_list = []
+    for ImageListStack_index, ImageListStack_image in enumerate(image_list):
+        array_list.append(np.array(cv2.imread(path + "/data/" + ImageListStack_image, cv2.IMREAD_GRAYSCALE)))
+        
+    return np.stack(array_list, axis=2)
+
+if __name__ == "__main__" and ( (len(sys.argv) <= 1) or ("plain_tensor" in sys.argv) ):     # Generates tensor data only if no terminal argv, or "tensor" argument
 
             image_list = ImageListGenerator(reduced_suite_id, reduced_sample_id, reduced_code)
             stacked_images_array = ImageListStack(data_folder_path, image_list)
 
             train_data_tensor = torch.from_numpy(stacked_images_array).to(torch.float32)
-            train_data_tensor = torch.nn.functional.normalize(train_data_tensor, dim=1)     # Norme 1
+            #train_data_tensor = torch.nn.functional.normalize(train_data_tensor, dim=1)     # Norme 1
+            train_data_tensor = train_data_tensor/255    # Normalized pixels
             torch.save(train_data_tensor, "train_data_tensor.pt")
             
             print("dtype | size:", train_data_tensor.dtype, "|", train_data_tensor.size())
             print("Train data mean, max :", torch.mean(train_data_tensor), torch.max(train_data_tensor))
- 
+if __name__ == "__main__" and ( (len(sys.argv) <= 1) or ("convoluted_tensor" in sys.argv) ):     # Generates tensor data only if no terminal argv, or "tensor" argument
+
+    image_list = ImageListGenerator(reduced_suite_id, reduced_sample_id, reduced_code)
+    stacked_images_array = ImageListStack(data_folder_path, image_list)
+
+    train_data_tensor = torch.from_numpy(stacked_images_array).to(torch.float32)
+    #train_data_tensor = torch.nn.functional.normalize(train_data_tensor, dim=1)     # Norme 1
+    train_data_tensor = train_data_tensor/255    # Normalized pixels
+    torch.save(train_data_tensor, "train_data_tensor.pt")
+    
+    print("dtype | size:", train_data_tensor.dtype, "|", train_data_tensor.size())
+    print("Train data mean, max :", torch.mean(train_data_tensor), torch.max(train_data_tensor))
+
+    fig = plt.subplots(4, 5)
+    plt.suptitle("Convoluted images")
+    for i in range(20):
+        plt.subplot(4, 5, i+1)
+        plt.imshow(train_data.numpy()[i,:].reshape(m,n))
+        plt.title( f"Real : {test_labels.numpy()[i]}")
+    plt.close('all')
+    plt.show()
+        
+
 # %%
 import torch.nn as nn
 
@@ -96,7 +127,7 @@ class DeepSimpleNN(nn.Module):
         )
 
     def forward(self, x):
-        x = self.flatten(x)
+        # x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
 
@@ -115,18 +146,36 @@ if __name__ == "__main__" and ( (len(sys.argv) <= 1) or ("supervised_model" in s
     input_size = m * n
     hidden_size = 128
     output_size = len(np.unique(reduced_value))
-    
+
+    fig = plt.subplots(4, 5)
+    plt.suptitle("Sliced train_data array")
+    for i in range(20):
+        plt.subplot(4, 5, i+1)
+        plt.imshow(train_data.numpy()[:,:,i])
+        plt.title( f"Real : {test_labels.numpy()[i]}")
+        
+    train_data = train_data.permute(2, 0, 1)
     train_data = train_data.view(z, m*n)
     print(train_data.size())
+    test_data = test_data.permute(2, 0, 1)
     test_data = test_data.view(z, m*n)
     print(test_data.size())
-    
+
+    fig = plt.subplots(4, 5)
+    plt.suptitle("Flatten reshaped train_data array")
+    for i in range(20):
+        plt.subplot(4, 5, i+1)
+        plt.imshow(train_data.numpy()[i,:].reshape(m,n))
+        plt.title( f"Real : {test_labels.numpy()[i]}")
+    plt.close('all')
+    plt.show()
+
     supervised_model = SupervisedSimpleNN(input_size, hidden_size, output_size)
     criterion = nn.CrossEntropyLoss()  
     optimizer = torch.optim.Adam(supervised_model.parameters(), lr=0.001)
 
     # Supervised model training
-    num_epochs = int(input("Number of epochs :"))
+    num_epochs = int(input("Number of epochs : "))
     batch_size = z
     for epoch in tqdm(range(num_epochs)):
         running_loss = 0.0
@@ -146,14 +195,15 @@ if __name__ == "__main__" and ( (len(sys.argv) <= 1) or ("supervised_model" in s
             
         # if epoch//10 == 0:
         #     print(f"Epoch {epoch+1}, Loss: {running_loss / len(train_data)}")
-
+    print("Final loss :", running_loss)
+    
     # Supervised model evaluation 
     supervised_model.eval()
     with torch.no_grad():
         correct = 0
         total = 0
 
-        for i in tqdm(range(0, len(test_data), batch_size)):
+        for i in (range(0, len(test_data), batch_size)):
             inputs = test_data[i:i+batch_size]
             labels = test_labels[i:i+batch_size]
 
@@ -161,9 +211,25 @@ if __name__ == "__main__" and ( (len(sys.argv) <= 1) or ("supervised_model" in s
             _, predicted = torch.max(outputs, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
+    
+    # Model testing
+    print(np.unique(predicted.numpy(), return_counts=True))
+    print(np.unique(test_labels.numpy(), return_counts=True))
 
-        print(f"Accuracy: {100 * correct / total}%")
-
+    print(f"Accuracy: {100 * correct / total}%")
+    
+    print(predicted.numpy()[test_labels.numpy() == 0])
+    print(np.unique(predicted.numpy()[test_labels.numpy() == 0], return_counts=True))
+    
+    fig = plt.subplots(4, 5)
+    for i in range(20):
+        plt.subplot(4, 5, i+1)
+        plt.imshow(test_data.numpy()[i, :].reshape(m, n))
+        plt.title( f"Predicted : {predicted.numpy()[i]}, \n Real : {test_labels.numpy()[i]}")
+    plt.close('all')
+    plt.show()
+    
+# %%
 
 if __name__ == "__main__" and ( (len(sys.argv) <= 1) or ("deep_model" in sys.argv) ):
     # Run model only if no terminal argv, or "model" argument        
@@ -182,41 +248,19 @@ if __name__ == "__main__" and ( (len(sys.argv) <= 1) or ("deep_model" in sys.arg
     print(DeepSimpleNN_model)
 
     input_array = input("Select image number to predict : ") 
-    train_data = torch.load(current_folder_path + "/train_data_tensor.pt")[:,:, int(input_array)]
+    train_data = torch.load(current_folder_path + "/train_data_tensor.pt")[:,:, int(input_array)].flatten()
+
     logits = DeepSimpleNN_model(train_data)
     pred_probab = nn.Softmax(dim=1)(logits)
     y_pred = pred_probab.argmax(1)
     print(f"Predicted class: {y_pred}")
     
-    
-    plt.imshow(torch.numpy(train_data))
-    print(torch.numpy(train_data))
-    plt.title(f"Predicted class : {y_pred}")
-    plt.show()
+    # plt.imshow(torch.numpy(train_data))
+    # plt.title(f"Predicted class : {y_pred}")
+    # plt.show()
     #plt.sleep(2)
-    plt.close('all')
+    #plt.close('all')
     
-    # numbersNN_1 = SimpleNN_1()
-    # criterion = nn.CrossEntropyLoss()
-    # optimizer = torch.optim.SGD(net.parameters(), lr=0.01)
-
-    # num_epochs = 5
-
-    # for epoch in range(num_epochs):
-    #     running_loss = 0.0
-    #     for i, data in enumerate(trainloader, 0):
-    #         inputs, labels = data
-
-    #         optimizer.zero_grad()
-
-    #         outputs = net(inputs)
-    #         loss = criterion(outputs, labels)
-    #         loss.backward()
-    #         optimizer.step()
-
-    #         running_loss += loss.item()
-
-    #     print(f"Epoch {epoch+1}, Loss: {running_loss / len(trainloader)}")
 
 # %%
 
@@ -247,5 +291,4 @@ class DataHandler:
 #     plt.title(k)
 # plt.show()
 
-print(len(sys.argv))
-print(__file__[__file__.rindex("\\")+1:], f"says : \033[1mscript ended successfully\033[0m")
+print(__file__[__file__.rindex("\\")+1:], f"says : \033[1mSCRIPT TERMINATED SUCCESSFULLY\033[0m")
